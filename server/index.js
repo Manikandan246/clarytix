4f27795c47d60f3e7504b473c9077fc06d04b94e
Manikandan <sanjeevmani246@gmail.com>
2025-05-05 00:42:16 +0530

    Removed static

--- server/index.js
+++ server/index.js
@@ -397,7 +397,7 @@ app.get('/admin/performance-metrics', async (req, res) => {
 // =====================
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
